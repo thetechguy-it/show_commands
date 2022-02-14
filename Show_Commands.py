5f080c4507ac9bc12caf1d28b2c6cce4904ec302
Riccardo Nalesso <99249452+rickynale@users.noreply.github.com>
2022-02-14 18:17:16 +0100

    Finish

--- Show_Commands.py
+++ Show_Commands.py
@@ -1,20 +1,23 @@
 # With this script you will launch the commands inside the file called 'Commands.txt' to all the devices inside the file called 'IPAddressList.txt
 
-
 from netmiko import ConnectHandler
 from contextlib import redirect_stdout
 import credentials
 import type
+import os
 
-
-total_ips = open("IPAddressList.txt", "r")
+total_ips = open("IPAddressList.txt", "r") # Create a file called "IPAddressList.txt" and put one IP addresses in each line
 ips = total_ips.readlines()
-total_commands = open("Commands.txt", "r")
+total_commands = open("Commands.txt", "r") # Create a file called "Commands.txt" and put one CLI commans in each line
 commands = total_commands.readlines()
+
 print("With this script you will launch the commands inside the file called 'Commands.txt' to all the devices inside the file called 'IPAddressList.txt")
+os.mkdir("CFG Backup")
+# Put your own folder
+os.chdir("") 
 
 for ip in ips:
-    SSH = ConnectHandler (ip, device_type = type.type_ios, username = credentials.username, password = credentials.password)
+    SSH = ConnectHandler (ip, device_type = type.type_ios, username = credentials.username, password = credentials.password, fast_cli = False)
     dev_name = SSH.send_command("show run | in hostname")
     dev_name = dev_name.split(" ")
     hostname = dev_name[1]
@@ -22,12 +25,11 @@ for ip in ips:
     SSH_Check_Connection = SSH.is_alive()
     print("The connection to the device is:" + str(SSH_Check_Connection))
     with open(hostname + ".txt", "w") as txt_file:
-        print("I created I file called: ", hostname,". You will find here all the outputs")
-        with redirect_stdout(txt_file):
-            for command in commands:
-                print("***", command, "***")
-                output = (SSH.send_command(command))
-                print (output, "\n\n")         
+        print("The file called: " + hostname +".txt has been created into the 'CFG Backup' folder. You will find here all the outputs")
+        for command in commands:
+            txt_file.write("This is the output for the following command:" + command)
+            output = (SSH.send_command(command))
+            txt_file.write(output + "\n\n")         
     SSH.disconnect()
     SSH_Check_Connection = SSH.is_alive()
     print("The connection to the device is:" + str(SSH_Check_Connection))
