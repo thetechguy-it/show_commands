2ab1c7a6312aa236f666741c3f9807773105377e
rickynale <riccardo.nalesso10@gmail.com>
2022-12-01 13:07:47 +0100

    fix and new scripts

---
+++ static_show_commands_create.py
@@ -0,0 +1,89 @@
+from netmiko import ConnectHandler
+import os_vendor
+import os
+from datetime import date, datetime
+from ping3 import ping
+import getpass
+
+def create_folder(folder):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    os.chdir(folder)
+
+# Login to the devices 
+def device_login(host, username, password):
+    cisco_ios = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': username,
+        'password': password,
+    }
+    try:
+        connection = ConnectHandler(**cisco_ios)
+        connection.enable()
+        device_hostname(connection) 
+    except Exception as err:
+        print(f"Oops! {err}")
+
+# Retrieve the device Hostname
+def device_hostname(SSH):
+    output = SSH.send_command("show run")
+    dev_name = SSH.send_command("show run | in hostname")
+    dev_name = dev_name.split(" ")
+    hostname = dev_name[1]
+    print("The device name is: ", hostname)
+    device_commands(hostname, SSH) 
+
+# Push the commands stored into the "Commands.txt" file and store the output in different files called with Device hostname and timestamp
+def device_commands(device_name, device_SSH):
+    with open(device_name + "_" + dt_string_full + ".txt", "w") as txt_file:
+        print("The file called: " + device_name + ".txt has been created into the " + backup_folder + "/" + dt_string + " folder. You will find here all the outputs. I'm going to close the SSH session")
+        for command in commands_list:
+            txt_file.write("######## " + command + " ######## \n\n")
+            output = (device_SSH.send_command(command))
+            txt_file.write(output + "\n\n\n\n")         
+        device_SSH.disconnect()
+        print("\n\n\n")
+
+# Verify if an IP address is reachable.
+# If YES: run "device_login" function
+# If NO: Create a file called with the IP Address of the device and write inside "This device is not reachable. Please verify its status and connectivity"
+def ip_reachability():
+    for ip in ip_list:
+        print("Testing: " + ip)
+        ip_reach = ping(ip)
+        if ip_reach == None:
+            print("The device with the IP Address: " + ip + " is not reachable. Please verify its status and connectivity\n\n\n")
+            with open(ip + "_" + dt_string_full + ".txt", "w") as downdevice:
+                downdevice.write("This device is not reachable. Please verify its status and connectivity")
+        else:
+            print ("It's reachable! I'm going to retrieve data from it.")
+            device_login(ip, username, password)
+
+
+# User enters device credentials 
+username = input('Enter your username: ')
+password = getpass.getpass('Enter your password: ')
+
+# Code opens the file where IP Addresses are stored (1 per line)
+file_ip = open("IPAddressList.txt", "r")
+total_ips = file_ip.read()
+ip_list = total_ips.splitlines()
+
+# Code opens the file where commands are stored (1 per line)
+file_commands = open("Commands.txt", "r")
+total_commands = file_commands.read()
+commands_list = total_commands.splitlines()
+
+# Create Backup Folder
+backup_folder = "BACKUP"
+create_folder(backup_folder)
+
+# Create daily folder inside Backup Folder
+now = datetime.now()
+dt_string = now.strftime("%m-%d-%Y")
+dt_string_full = now.strftime("%m-%d-%Y_%H-%M-%S")
+create_folder(dt_string)
+
+# Code
+ip_reachability()
